0ab012b82069c06bd0cc8988e33ef809986df117
quantumiracle <zhding@mail.ustc.edu.cn>
2022-07-25 23:22:30 -0400

    fix

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
+mkdir -p log/$DATE
 
 nohup python launch.py --env pettingzoo_boxing_v2 --method nash_dqn --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/boxing_v2_nash_dqn.log &
